97472891520f68358690aa43655517762b3a85e1
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-04-03 14:25:34 +0900

    server client update: client file
    클라이언트 파일 복붙 잘못해서 이상했음.

--- oracle-server/client.py
+++ oracle-server/client.py
@@ -1,21 +1,5 @@
+import os
 import socket, threading
-import os
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 5000))
-nickname = input("Choose your nickname: ")
-
-
-
-def getFileSize(file):
-    return os.path.getsize(file)
-
-def getFileData(file_t):
-    with open(file_t, 'rb') as file:  # 인코딩 인자 제거
-        data = b""  # 이진 데이터로 저장
-        for line in file:
-            data += lineimport socket, threading
-import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('152.67.212.55', 5000))
@@ -68,56 +52,6 @@ def write():
         else:
             message = '{}: {}'.format(nickname, message)
             client.send(message.encode('ascii'))
-
-# 멀티 클라이언트용 쓰레드
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-# 메시지 보내기
-write_thread = threading.Thread(target=write)
-write_thread.start()
-    return data
-
-def send_file(file_path):
-    try:
-        file_name = os.path.basename(file_path)
-        file_size = os.path.getsize(file_path)
-        # 파일 정보 전송
-        client.send(f"file {file_name}".encode('ascii'))
-        client.recv(1024)  # 서버가 준비되었다는 응답을 받음
-        client.send(str(file_size).encode('ascii'))
-        # 파일 데이터 전송
-        with open(file_path, 'rb') as file:
-            data = file.read(1024)
-            while data:
-                client.send(data)
-                data = file.read(1024)
-        print("File sent successfully.")
-    except FileNotFoundError:
-        print("File not found.")
-    except Exception as e:
-        print("An error occurred while sending the file:", e)
-
-
-def receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'Ready to receive file':
-                send_file("/home/oracle/oracle-server/text.txt")
-            elif message == 'NICKNAME':
-                client.send(nickname.encode('ascii'))
-            else:
-                print(message)
-        except:
-            print("An error occured!")
-            client.close()
-            break
-
-def write():
-    while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
 
 # 멀티 클라이언트용 쓰레드
 receive_thread = threading.Thread(target=receive)
