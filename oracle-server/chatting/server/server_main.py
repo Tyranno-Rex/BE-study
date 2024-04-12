bca76784eb02f30ab86b5d85f953c001f0ac7399
eunseong <jsilvercastle@gmail.com>
2024-04-12 17:04:53 +0900

    oracle-server update: h264-streaming update -> client get video list and watch the video by selection

--- oracle-server/chatting/server/server_main.py
+++ oracle-server/chatting/server/server_main.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
+import cv2, os
 import socket, threading, sys, requests
-import signal, json
-from collections import deque
+import signal
 
 import server_function as sf
 import server_message_handler as smh
@@ -67,6 +67,50 @@ def signal_handler(sig, frame):
 
 # 시그널 핸들러 등록
 signal.signal(signal.SIGINT, signal_handler)
+
+
+
+
+
+
+
+
+
+
+def generate_frames(video_path):
+    video = cv2.VideoCapture(video_path)
+    while True:
+        success, frame = video.read()
+        if not success:
+            break
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# 동영상 이름도 받아 올거임
+@app.route('/video_feed')
+def video_feed():
+    video_name = request.args.get('video_name')
+    video_path = 'C:/Users/admin/project/oracle/oracle-server/h264/database/' + video_name
+    return Response(generate_frames(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_frame_count')
+def get_frame_count():
+    video_name = request.args.get('video_name')
+    video_path = 'C:/Users/admin/project/oracle/oracle-server/h264/database/' + video_name
+    video = cv2.VideoCapture(video_path)
+    frame_count = video.get(cv2.CAP_PROP_FPS)
+    return {'frame_count': frame_count}
+
+@app.route('/get_video_list')
+def get_video_list():
+    video_list = os.listdir('C:/Users/admin/project/oracle/oracle-server/h264/database')
+    return {'video_list': video_list}
+
+
+
+
 
 # Flask 서버 시작
 if __name__ == '__main__':
